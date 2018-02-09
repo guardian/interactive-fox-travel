fb13845bda24ee9a37f8e8acb62d0b6a1d9e0daf
seanclarkeguardian <sean.clarke@guardian.co.uk>
2018-02-09 10:41:15 +0000

    now with wobbly seesaws

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,23 @@ var el = document.createElement('script');
 
 el.src = process.env.PATH + '/app.js';
 document.body.appendChild(el);
+
+var charts = [].slice.apply(document.querySelectorAll('.gv-trip-chart'));
+
+function wobble(c) {
+    c.setAttribute("style", "transform:rotate(" + c.getAttribute("data-angle") + ")");
+}
+
+function checkIsInView(el){
+    var bbox = el.getBoundingClientRect();
+    var viewh = window.innerHeight;
+    if (bbox.top > 0 && bbox.top < (viewh - (viewh/2))) {
+        wobble(el);
+    }
+} 
+
+charts.forEach(function(c){
+    window.addEventListener("scroll",function(){
+        checkIsInView(c);
+    })
+})
