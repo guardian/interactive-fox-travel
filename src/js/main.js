1cb15bbe9ea3a059267e225699203aed5b4af9fc
seanclarkeguardian <sean.clarke@guardian.co.uk>
2018-02-09 11:12:24 +0000

    publishing preview

--- src/js/main.js
+++ src/js/main.js
@@ -9,12 +9,19 @@ function wobble(c) {
     c.setAttribute("style", "transform:rotate(" + c.getAttribute("data-angle") + ")");
 }
 
+function unwobble(c) {
+    c.setAttribute("style", "transform:rotate(0deg)")
+}
+
 function checkIsInView(el){
     var bbox = el.getBoundingClientRect();
     var viewh = window.innerHeight;
     if (bbox.top > 0 && bbox.top < (viewh - (viewh/2))) {
         wobble(el);
+    } else if (bbox.bottom < 0) {
+        unwobble(el);
     }
+
 } 
 
 charts.forEach(function(c){
